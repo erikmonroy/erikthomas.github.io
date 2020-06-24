ece8a844faa6a3a7a23002637605e6bbd78df025
erikthomas <espangenberg@vassar.edu>
2020-06-24 16:45:55 -0400

    data created

--- game.js
+++ game.js
@@ -1,14 +1,22 @@
+//BG DATA
 createBG = function(){
   bg = Math.ceil(Math.random()*40+80)
   return bg
 }
-
 createNextBG = function(bg){
   return Math.ceil(jStat.normal.sample(bg,3))
 }
+let data = []
+bg1 = createBG()
+bg2 = createNextBG(bg1)
+data.push(bg1)
+data.push(bg2)
+for (let i = 2; i < 49; i++){
+  data.push(createNextBG(data[i-1]))
+}
+console.log(data)
 
-
-
+//HIDDEN MARKOV MODEL
 const stableProb = [0.8,0.2]
 const fallingProb = [0.1,0.9]
 const risingProb = [0.2,0.8]
