03149833203cb3a1317fbc5c015539c964f818d9
Erik Spangenberg <espangenberg@vassar.edu>
2020-08-17 23:23:44 -0400

    Static graph & OOP rewrite

--- game.js
+++ game.js
@@ -1,21 +1,29 @@
 //BG DATA
-createBG = function(){
-  bg = Math.ceil(Math.random()*40+80)
-  return bg
-}
-createNextBG = function(bg){
-  return Math.ceil(jStat.normal.sample(bg,3))
-}
-let data = []
-bg1 = {x:1,y:createBG()}
-bg2 = {x:2,y:createNextBG(bg1.y)}
-data.push(bg1)
-data.push(bg2)
+//Functions
+createValue = function()  {
+  return Math.ceil(Math.random()*40+80)
+};
+createNextValue = function(bg) {
+  return Math.ceil(jStat.normal.sample(bg,3)) 
+};
+
+//BG Constructor
+function BG(index, value, state) {
+  this.index = index;
+  this.value = value;
+  this.state = state;
+};
+
+//BG data
+let bgData = []
+let bg1 = new BG(1,createValue(),"stable");
+let bg2 = new BG(2,createNextValue(bg1.value),"stable");
+bgData.push(bg1)
+bgData.push(bg2)
 for (let i = 2; i < 50; i++){
-  bg3= {x:(i+1),y:createNextBG(data[i-1].y)}
-  data.push(bg3)
+  bg3= new BG(i+1,createNextValue(bgData[i-1].value),"stable");
+  bgData.push(bg3)
 }
-console.log(data)
 
 // //HIDDEN MARKOV MODEL
 // const stableProb = [0.8,0.2]
@@ -25,59 +33,73 @@ console.log(data)
 // console.log(transProb)
 
 //d3 viz
+//1. PREP
+//SVG
+const width = 960;
+const height = 500;
+const margin = 5;
+const padding = 5;
+const adj = 30;
 
-//dynamic function to resize SVG
-// function responsivefy(svg) {
-//   const container = d3.select(svg.node().parentNode),
-//       width = parseInt(svg.style('width'), 10),
-//       height = parseInt(svg.style('height'), 10),
-//       aspect = width / height;
-//   svg.attr('viewBox', `0 0 ${width} ${height}`)
-//   .attr('preserveAspectRatio', 'xMinYMid')
-//   .call(resize);
-//   d3.select(window).on(
-//     'resize.' + container.attr('id'), 
-//     resize
-//     );
-//   function resize() {
-//     const w = parseInt(container.style('width'));
-//     svg.attr('width', w);
-//     svg.attr('height', Math.round(w / aspect));
-//   }
-// }
+const svg = d3.select("div#container").append("svg")
+    .attr("preserveAspectRatio", "xMinYMin meet")
+    .attr("viewBox", "-"
+          + adj + " -"
+          + adj + " "
+          + (width + adj *3) + " "
+          + (height + adj*3))
+    .style("padding", padding)
+    .style("margin", margin)
+    .classed("svg-content", true);
 
-//parameters
-let height = 300
+//SCALES
+const xScale = d3.scaleLinear().range([0,width]);
+const yScale = d3.scaleLinear().rangeRound([height, 0]);
+xScale.domain([1,50]);
+yScale.domain([40,400]);
 
-// create scales    
-//let x_slices = [40,100,200,300,400]
-let y_scale = d3.scaleLinear()
-    //.domain = ([d3.min(slices),d3.max(slices)])
-    .domain = ([30,400])
-    .range = ([height,0]);
+//AXES
+const yaxis = d3.axisLeft().scale(yScale); 
+const xaxis = d3.axisBottom().scale(xScale);
 
-//create SVG element
-let svg = d3.select('.graph')
-    .append("svg")
-    .attr("height","100%")
-    .attr("width","100%")
+//LINES
+const line = d3.line()
+    .x(function(d) { return xScale(d.index); })
+    .y(function(d) { return yScale(d.value); });
 
-//create line
-let line = d3.line()
-    .x(function (d,i){  return d.x})
-    .y(function (d,i){  return height - d.y})
-    .curve(d3.curveCardinal);
+//2. DRAW
+//AXES
+svg.append("g")
+    .attr("class", "axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xaxis);
 
-svg.append("path")
-    .attr("fill","none")
-    .attr("stroke","black")
-    .attr("d",line(data));
+svg.append("g")
+    .attr("class", "axis")
+    .call(yaxis)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("dy", ".75em")
+    .attr("y", 6)
+    .style("text-anchor", "end")
+    .text("Blood Glucose");
 
-//create dots
-svg.selectAll("circle")
-    .data(data)
+//LINES
+svg.append("path").datum(bgData)
+.attr("fill", "none")
+.attr("stroke", "black")
+.attr("stroke-linejoin", "round")
+.attr("stroke-linecap", "round")
+.attr("stroke-width", 1.5)
+.attr("d", line);
+
+//DOTS
+svg.append("g")
+    .selectAll('dot')
+    .data(bgData)
     .enter()
     .append("circle")
-    .attr("cx",function (d,i){  return d.x})
-    .attr("cy",function (d,i){  return height - d.y})
-    .attr("r","0.75")
+      .attr("cx", function (d)  {return xScale(d.index)})
+      .attr("cy", function (d)  {return yScale(d.value)})
+      .attr("r", 1.5)
+      .style("fill", "black")
