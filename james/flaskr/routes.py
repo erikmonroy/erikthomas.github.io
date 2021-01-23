28632658b45c186cf7449461095cd1a5826c3710
Erik <44878505+erikspang@users.noreply.github.com>
2021-01-23 11:43:01 -0500

    simply flask

--- james/flaskr/routes.py
+++
@@ -1,7 +0,0 @@
-# Route declaration.
-from flask import current_app as app
-from flask import render_template
-
-@app.route('/')
-def index():
-    return render_template("index.html")
---
+++ james/james.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import render_template
+
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+@app.route('/')
+def index():
+    return render_template("index.html")
