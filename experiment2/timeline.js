59dae6ee9fc8b698c9444947b194552ded558fa4
Erik <44878505+erikspang@users.noreply.github.com>
2021-04-15 00:50:08 -0400

    task done, jspsych init

---
+++ experiment2/timeline.js
@@ -0,0 +1,25 @@
+/* create timeline */
+var timeline = [];
+
+/* define welcome message trial */
+
+var instructions = {
+    type: 'instructions',
+    pages: [
+        'Welcome to the experiment. Click next to begin.',
+        `In this experiment, a circle will appear in the center 
+        of the screen. Your objective is to keep the circle within range using grow and shrink buttons.`,
+        'Here is a picture of the circle in an acceptable range: <img src="dot_ir.jpg"></img>',
+        `If the circle shrinks out of range, use the grow button to increase it: <img src="dot_lo.jpg"`,
+        `If the circle grows out of range, use the shrink button to decrease it: <img src="dot_hi.jpg"`,
+        `Following the trial, you will answer some questions about your experience. Good luck!`
+    ],
+    show_clickable_nav: true
+}
+
+timeline.push(instructions);
+
+/* start the experiment */
+jsPsych.init({
+    timeline: timeline
+});
